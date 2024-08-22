d5ed3ab1ff9b8c240bc266a7701d8b644c8b4b79
Aur316 <kristianfeher86@gmail.com>
2024-08-22 13:24:58 +0200

    security: X-Frame;X-Content;X-XXX

--- next.config.js
+++ next.config.js
@@ -1,6 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
 
-module.exports = nextConfig
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self'; script-src 'self'; object-src 'none'; frame-ancestors 'none';",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
